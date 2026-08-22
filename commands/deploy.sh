f32dccb903061efdf25522d03e8ee40becec8669
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-08-22 18:57:59 +0200

    Add CI and CD pipelines

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-# Exit the script immediately if any command exits with a non-zero status
 set -e
 
-# Function to handle errors with custom messages
+PROJECT_DIR="/home/ubuntu/src/py-fastapi-homework-5-ec2-deploy-task"
+DEPLOY_BRANCH="develop"
+
 handle_error() {
     echo "Error: $1"
     exit 1
 }
 
-# Navigate to the application directory
-cd /home/ubuntu/src/mate-fastapi-homework-5 || handle_error "Failed to navigate to the application directory."
+cd "$PROJECT_DIR" || handle_error "Failed to navigate to the application directory."
 
-# Fetch the latest changes from the remote repository
 echo "Fetching the latest changes from the remote repository..."
-git fetch origin main || handle_error "Failed to fetch updates from the 'origin' remote."
+git fetch origin "$DEPLOY_BRANCH" || handle_error "Failed to fetch updates from the 'origin' remote."
 
-# Reset the local repository to match the remote 'main' branch
-echo "Resetting the local repository to match 'origin/main'..."
-git reset --hard origin/main || handle_error "Failed to reset the local repository to 'origin/main'."
+echo "Switching to '$DEPLOY_BRANCH' and synchronizing with 'origin/$DEPLOY_BRANCH'..."
+git checkout "$DEPLOY_BRANCH" || handle_error "Failed to switch to '$DEPLOY_BRANCH'."
+git reset --hard "origin/$DEPLOY_BRANCH" || handle_error "Failed to reset the local repository to 'origin/$DEPLOY_BRANCH'."
 
-# (Optional) Pull any new tags from the remote repository
 echo "Fetching tags from the remote repository..."
 git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
 
-# Build and run Docker containers with Docker Compose v2
-docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
+sudo docker compose -f docker-compose-prod.yml up -d --build --remove-orphans || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
 
-# Print a success message upon successful deployment
 echo "Deployment completed successfully."
